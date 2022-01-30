74f30d856e2f1d85fc23253aff22124ab54dd988
Vincent Mahnke <vincent@mahn.ke>
2022-01-30 19:29:05 +0100

    Implement logic to swipe to open event drawer

--- src/components/GDQHeader.vue
+++ src/components/GDQHeader.vue
@@ -37,7 +37,76 @@ export default {
       container!.addEventListener('MDCTopAppBar:nav', () => {
           drawer.value!.open = !drawer.value!.open;
       });
+      setupSwipeLogic(drawer.value!);
     });
+
+    const setupSwipeLogic = (drawer : TopAppBarFixedWithOpen) => {
+      let touchIdentifier = -1;
+      const veloEstimate = 10;
+      const clientXThreshold = 20;
+      let velocity : Touch[] = [];
+      const updateVelocity = (touch : Touch) => {
+          if(velocity.length >= veloEstimate)
+          {
+              velocity.splice(0,1);
+          }
+          velocity.push(touch);
+          console.log(calculateVelocity())
+      };
+      const calculateVelocity = () => {
+          let average = 0;
+          if (velocity.length == 1)
+          {
+              return average;
+          }
+          for (let i : number = 1; i < velocity.length; i++)
+          {
+              average += velocity[i].clientX - velocity[i-1].clientX;
+          }
+          average /= velocity.length;
+          return average;
+      };
+      window.addEventListener("touchstart", (touchStartEvent) => {
+          if (touchIdentifier != -1)
+          {
+              return;
+          }
+          if (!touchStartEvent.touches[0])
+          {
+              return;
+          }
+          if (!drawer!.open)
+          {
+              if (touchStartEvent.touches[0].clientX > clientXThreshold)
+              {
+                  return;
+              }
+          }
+          touchIdentifier = touchStartEvent.touches[0].identifier;
+      });
+      window.addEventListener("touchmove", (touchMoveEvent) => {
+          const touchUpdate = Array.from(touchMoveEvent.touches).find(touch => touch.identifier == touchIdentifier);
+          if (!touchUpdate)
+          {
+              return;
+          }
+          updateVelocity(touchUpdate);
+      })
+      window.addEventListener("touchend", (touchEndEvent) => {
+          if (touchIdentifier == -1)
+          {
+              return;
+          }
+          if (Array.from(touchEndEvent.touches).find(touch => touch.identifier == touchIdentifier))
+          {
+              return;
+          }
+          drawer!.open = calculateVelocity() > 0
+          velocity =[]
+          touchIdentifier = -1;
+      })
+    };
+
     const currentEvent = ref("");
     const runs = ref<[pk : number, data : GDQRunData][]>([]);
     const reminder = ref<number[]>([]);
@@ -119,8 +188,8 @@ export default {
               <mwc-list activatable multi>
                 <template v-for="[runPK, runData] in runs" :key="runPK">
                   <mwc-list-item twoline @click="toggleReminder(runPK)">
-                    <span>{{runData.display_name}}</span>
-                    <span slot="secondary">{{runData.deprecated_runners}}</span>
+                    <span>{{runData.fields.display_name}}</span>
+                    <span slot="secondary">{{runData.fields.deprecated_runners}}</span>
                   </mwc-list-item>
                   <li divider role="separator" padded></li>
                 </template>
