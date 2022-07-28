0e784305a96d8410dc102d743ac688a5366a96c0
Vincent Mahnke <vincent@mahn.ke>
2022-07-28 16:11:02 +0200

    fix(android): Bump SDK version fix(android): Ensure `Version` plugin is registered before base activity `onCreate()` is called

--- android/app/src/main/java/ke/mahn/gdqreminder/MainActivity.java
+++ android/app/src/main/java/ke/mahn/gdqreminder/MainActivity.java
@@ -6,7 +6,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         registerPlugin(VersionPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }
