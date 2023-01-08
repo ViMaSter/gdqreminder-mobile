f1579d37a2ed41dfdfd7142f0656fb9b6154af90
Vincent Mahnke <vincent@mahn.ke>
2023-01-08 11:15:39 +0000

    feat: Add sentry feat: Generate version code and version name from package.json fix: tag based on package.json instead of build.gradle

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
-import { createApp, watch } from 'vue'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+import {version} from '../package.json';
+
 
 import App from './App.vue'
 
 const app = createApp(App)
 const pinia = createPinia();
+
+Sentry.init({
+  app,
+  dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
+  integrations: [
+    new BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+  release: "gdqreminder-mobile@" + version,
+});
+
 app.use(pinia);
 app.mount('#app')
