37c27006ca308a1d53320b8de8f288182c9d16bf
Vincent <vincent@mahn.ke>
2023-01-13 22:48:31 +0100

    fix: Disable Sentry locally

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,17 @@ import App from './App.vue'
 const app = createApp(App)
 const pinia = createPinia();
 
-Sentry.init({
-  app,
-  dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
-  integrations: [
-    new BrowserTracing(),
-  ],
-  tracesSampleRate: 1.0,
-  release: "gdqreminder-mobile@" + version,
-});
-
+if (process.env.SENTRY_AUTH_TOKEN && process.env.SENTRY_RELEASE)
+{
+  Sentry.init({
+    app,
+    dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
+    integrations: [
+      new BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0,
+    release: "gdqreminder-mobile@" + version,
+  });
+}
 app.use(pinia);
 app.mount('#app')
