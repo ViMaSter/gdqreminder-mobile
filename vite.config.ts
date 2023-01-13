c3a5b472730a156b55e3a053311ca14240c1b18a
Vincent <vincent@mahn.ke>
2023-01-13 03:11:17 +0100

    feat: Add sentry source plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,39 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
+import sentryVitePlugin from "@sentry/vite-plugin";
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue({
+
+const plugins = [
+  vue({
     template: {
       compilerOptions: {
         isCustomElement: tag => tag.startsWith('mwc-')
       }
     }
-  }), vueJsx()],
+  }),
+  vueJsx()
+];
+if (process.env.SENTRY_AUTH_TOKEN && process.env.SENTRY_RELEASE) {
+    sentryVitePlugin({
+      org: "vimaster",
+      project: "gdqtracker-mobile",
+
+      include: "./dist",
+
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      release: process.env.SENTRY_RELEASE,
+    });
+}
+
+export default defineConfig({
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
