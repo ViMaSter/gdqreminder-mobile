0e40c0bb3cb226ccd5dd3cc5b63a259eb90238f3
Vincent <vincent@mahn.ke>
2023-01-14 07:26:58 +0100

    fix: App not starting

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,15 @@ import App from './App.vue'
 const app = createApp(App)
 const pinia = createPinia();
 
-  Sentry.init({
-    app,
-    dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
-    integrations: [
-      new BrowserTracing(),
-    ],
-    tracesSampleRate: 1.0,
-    release: "gdqreminder-mobile@" + version,
-  });
+Sentry.init({
+  app,
+  dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
+  integrations: [
+    new BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+  release: "gdqreminder-mobile@" + version,
+});
+
 app.use(pinia);
 app.mount('#app')
