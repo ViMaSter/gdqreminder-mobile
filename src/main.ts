e225af921bd79b2db1ef8ae968355d04a6c1856c
Vincent <vincent@mahn.ke>
2023-01-14 18:11:22 +0100

    fix: Disable Sentry when running on desktop

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,24 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as Sentry from "@sentry/vue";
 import { BrowserTracing } from "@sentry/tracing";
-import {version} from '../package.json';
-
 
 import App from './App.vue'
+import { Capacitor } from '@capacitor/core';
 
 const app = createApp(App)
 const pinia = createPinia();
 
-Sentry.init({
-  app,
-  dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
-  integrations: [
-    new BrowserTracing(),
-  ],
-  tracesSampleRate: 1.0
-});
+// disable sentry in dev mode
+if (Capacitor.getPlatform() !== 'web') {
+  Sentry.init({
+    app,
+    dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
+    integrations: [
+      new BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0
+  });
+}
 
 app.use(pinia);
 app.mount('#app')
