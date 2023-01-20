dc817cbb63dfdda3843ae1facfb62346ac668462
Vincent Mahnke <vincent@mahn.ke>
2023-01-20 05:30:30 +0000

    fix(vite): Address breaking change in @sentry/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
-import sentryVitePlugin from "@sentry/vite-plugin";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
