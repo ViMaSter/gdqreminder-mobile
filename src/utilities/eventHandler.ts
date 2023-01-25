41b7e67e66c9c4940d7e1a88df17d40dc7d7a30c
Vincent Mahnke <vincent@mahn.ke>
2023-01-25 21:23:14 +0000

    chore: Formatting

--- src/utilities/eventHandler.ts
+++ src/utilities/eventHandler.ts
@@ -1,22 +1,18 @@
-export type NestedEventCallbacks = {[key : string] : Function | NestedEventCallbacks};
-export class EventHandler
-{
-    static handleCustomEvent(fullPath : string, nestedEventCallbacks : NestedEventCallbacks) {
+export type NestedEventCallbacks = { [key: string]: Function | NestedEventCallbacks };
+export class EventHandler {
+    static handleCustomEvent(fullPath: string, nestedEventCallbacks: NestedEventCallbacks) {
         // iterate over eventPath until function is found and call it with remaining eventPath items
         let eventPath = fullPath.split(".");
-        let currentElement : string;
-        do 
-        {
+        let currentElement: string;
+        do {
             currentElement = eventPath.shift()!;
-            if (typeof nestedEventCallbacks[currentElement] == "undefined")
-            {
-            break;
+            if (typeof nestedEventCallbacks[currentElement] == "undefined") {
+                break;
             }
-            
-            if (typeof nestedEventCallbacks[currentElement] == "function")
-            {
-            (nestedEventCallbacks[currentElement] as Function)(eventPath);
-            return;
+
+            if (typeof nestedEventCallbacks[currentElement] == "function") {
+                (nestedEventCallbacks[currentElement] as Function)(eventPath);
+                return;
             }
             nestedEventCallbacks = nestedEventCallbacks[currentElement] as NestedEventCallbacks;
         } while (eventPath.length > 0);
