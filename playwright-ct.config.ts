e2b0156dea6fd7d3138f200b6f9dcef6ba002bd7
Vincent Mahnke <vincent@mahn.ke>
2024-01-11 22:06:18 +0000

    fix: Addresses vite warning

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig, devices } from '@playwright/experimental-ct-vue';
 import { resolve } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 /**
  * See https://playwright.dev/docs/test-configuration.
