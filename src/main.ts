b488b420991d79608fee2a372da641a74ce1d2da
Vincent Mahnke <vincent@mahn.ke>
2025-05-24 15:54:31 +0200

    chore: Remove duplicate slot assignment

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { Capacitor } from "@capacitor/core";
 import { SafeArea } from "capacitor-plugin-safe-area";
 import { createI18n } from 'vue-i18n'
 import { Device } from '@capacitor/device';
-
+  
 const responseEn = await fetch("/i18n/en.json");
 if (!responseEn.ok) {
   throw new Error(`Failed to load locale file: en`);
