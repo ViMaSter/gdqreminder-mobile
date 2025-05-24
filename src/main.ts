2bb18f8ad7c18899e56517672269bb0ff7081a3a
Vincent Mahnke <vincent@mahn.ke>
2025-05-24 15:52:35 +0200

    chore: Reduced nesting for localization error handling during initialization

--- src/main.ts
+++ src/main.ts
@@ -6,70 +6,66 @@ import App from "./App.vue";
 import { Capacitor } from "@capacitor/core";
 import { SafeArea } from "capacitor-plugin-safe-area";
 import { createI18n } from 'vue-i18n'
-import { Device, GetLanguageCodeResult } from '@capacitor/device';
-export const SUPPORT_LOCALES = ['en', 'de', 'fr', 'it', 'ja'];
+import { Device } from '@capacitor/device';
 
-Device.getLanguageCode().then(async (result : GetLanguageCodeResult) => {
-  // fetch new URL('/i18n/${locale}.json', import.meta.url)
-  console.log("Device Language Code:", result.value);
-  let locale = "";
-  try {
-    const response = await fetch(`/i18n/${result.value}.json`);
-    if (!response.ok) {
-      throw new Error(`Failed to load locale file: ${result.value}`);
-    }
-    locale = await response.json();
-  }
-  catch (e) {
-    console.error(`Failed to load locale file: ${result.value}`, e);
-  }
+const responseEn = await fetch("/i18n/en.json");
+if (!responseEn.ok) {
+  throw new Error(`Failed to load locale file: en`);
+}
+const messages : {[code: string]: any } = {
+  en: await responseEn.json()
+};
 
-  const responseEn = await fetch("/i18n/en.json");
-  if (!responseEn.ok) {
-    throw new Error(`Failed to load locale file: en`);
+let languageCode = "en";
+try {
+  const languageCode = (await Device.getLanguageCode()).value;
+  const response = await fetch(`/i18n/${languageCode}.json`);
+  if (!response.ok) {
+    throw new Error(`Failed to load locale file: ${languageCode}`);
   }
-  const en = await responseEn.json();
+  const locale = await response.json();
 
-  const messages : {[code: string]: any } = {en};
   if (locale) {
-    messages[result.value] = locale;
+    throw new Error(`Locale file for ${languageCode} is empty`);
   }
+  messages[languageCode] = locale;
+} catch (error) {
+  console.warn(`Using fallback locale 'en' due to error: ${error}`);
+}
 
-  const i18n = createI18n({
-    
-    locale: result.value,
-    fallbackLocale: 'en',
-    messages
-  });
-  const app = createApp(App);
-  app.use(i18n)
-
-  const pinia = createPinia();
-
-  // disable sentry in dev mode
-  if (Capacitor.getPlatform() !== "web") {
-    Sentry.init({
-      app,
-      dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
-      replaysSessionSampleRate: 1.0,
-      replaysOnErrorSampleRate: 1.0,
-      integrations: [
-        Sentry.browserTracingIntegration(),
-        Sentry.replayIntegration({
-          maskAllText: false,
-          maskAllInputs: false,
-          networkDetailAllowUrls: [/.*/],
-        }),
-        Sentry.captureConsoleIntegration({
-          levels: ["error"],
-        }),
-      ],
-      tracesSampleRate: 1.0,
-    });
-  }
-
-  app.use(pinia);
-  app.mount("#app");
-
-  SafeArea.setImmersiveNavigationBar();
+const i18n = createI18n({
+  locale: languageCode,
+  fallbackLocale: 'en',
+  messages
 });
+const app = createApp(App);
+app.use(i18n)
+
+const pinia = createPinia();
+
+// disable sentry in dev mode
+if (Capacitor.getPlatform() !== "web") {
+  Sentry.init({
+    app,
+    dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        maskAllInputs: false,
+        networkDetailAllowUrls: [/.*/],
+      }),
+      Sentry.captureConsoleIntegration({
+        levels: ["error"],
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
+
+app.use(pinia);
+app.mount("#app");
+
+SafeArea.setImmersiveNavigationBar();
