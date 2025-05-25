bb8c91bb0152f741cbdeb952f7665fa462c6923d
Vincent Mahnke <vincent@mahn.ke>
2025-05-25 18:15:18 +0200

    chore(tests): Makes main.ts no longer a top-level await, resolving Playwright issue

--- src/main.ts
+++ src/main.ts
@@ -19,50 +19,52 @@ async function fetchLocale(languageCode: string): Promise<any> {
   }
   return locale;
 }
-const messages: { [code: string]: any } = {
-  en: await fetchLocale("en"),
-};
-let languageCode = "en";
-try {
-  languageCode = (await Device.getLanguageCode()).value;
-  messages[languageCode] = await fetchLocale(languageCode);
-} catch (error) {
-  console.warn(`Using fallback locale 'en' due to error: ${error}`);
-}
+(async () => {
+  const messages: { [code: string]: any } = {
+    en: await fetchLocale("en"),
+  };
+  let languageCode = "en";
+  try {
+    languageCode = (await Device.getLanguageCode()).value;
+    messages[languageCode] = await fetchLocale(languageCode);
+  } catch (error) {
+    console.warn(`Using fallback locale 'en' due to error: ${error}`);
+  }
 
-const i18n = createI18n({
-  locale: languageCode,
-  fallbackLocale: 'en',
-  messages
-});
-const app = createApp(App);
-app.use(i18n)
-
-const pinia = createPinia();
-
-// disable sentry in dev mode
-if (Capacitor.getPlatform() !== "web") {
-  Sentry.init({
-    app,
-    dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
-    replaysSessionSampleRate: 1.0,
-    replaysOnErrorSampleRate: 1.0,
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration({
-        maskAllText: false,
-        maskAllInputs: false,
-        networkDetailAllowUrls: [/.*/],
-      }),
-      Sentry.captureConsoleIntegration({
-        levels: ["error"],
-      }),
-    ],
-    tracesSampleRate: 1.0,
+  const i18n = createI18n({
+    locale: languageCode,
+    fallbackLocale: 'en',
+    messages
   });
-}
+  const app = createApp(App);
+  app.use(i18n)
 
-app.use(pinia);
-app.mount("#app");
+  const pinia = createPinia();
 
-SafeArea.setImmersiveNavigationBar();
+  // disable sentry in dev mode
+  if (Capacitor.getPlatform() !== "web") {
+    Sentry.init({
+      app,
+      dsn: "https://7cc331a1d01f4e78a7aab7e341705060@o489289.ingest.sentry.io/6470411",
+      replaysSessionSampleRate: 1.0,
+      replaysOnErrorSampleRate: 1.0,
+      integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration({
+          maskAllText: false,
+          maskAllInputs: false,
+          networkDetailAllowUrls: [/.*/],
+        }),
+        Sentry.captureConsoleIntegration({
+          levels: ["error"],
+        }),
+      ],
+      tracesSampleRate: 1.0,
+    });
+  }
+
+  app.use(pinia);
+  app.mount("#app");
+
+  SafeArea.setImmersiveNavigationBar();
+})();
