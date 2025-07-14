a9486317b180266089b28b3118fd263de64b8a87
Vincent Mahnke <vincent@mahn.ke>
2025-07-14 14:38:07 +0000

    feat(settings): Adds language selector

---
+++ src/stores/settings.ts
@@ -0,0 +1,80 @@
+import { defineStore } from "pinia";
+import PushNotificationHelper from '@/utilities/pushNotificationHelper';
+
+export const languages = {
+  'systemDefault': 'systemDefault',
+  'english': 'en-US',
+  'german': 'de-DE'
+} as const;
+export type LanguageKey = keyof typeof languages;
+
+type SettingsState = {
+  subscribedToEventAnnouncements: boolean;
+  subscribedToEventUpdates: boolean;
+  selectedLanguage: LanguageKey;
+};
+
+const key = "settings";
+const defaultValue: SettingsState = {
+  subscribedToEventAnnouncements: false,
+  subscribedToEventUpdates: false,
+  selectedLanguage: 'systemDefault',
+};
+
+export const useSettingsStore = defineStore(key, {
+  state: (): SettingsState => {
+    const state = localStorage.getItem("piniaState-" + key);
+    if (!state) {
+      return defaultValue;
+    }
+
+    return JSON.parse(state);
+  },
+  getters: {
+    eventAnnouncementsEnabled: (state) => state.subscribedToEventAnnouncements,
+    eventUpdatesEnabled: (state) => state.subscribedToEventUpdates,
+    currentLanguage: (state) => {
+      if (state.selectedLanguage !== 'systemDefault') {
+        return languages[state.selectedLanguage];
+      }
+      return navigator.language; // Fallback to browser language if 'systemDefault'
+    },
+  },
+  actions: {
+    async setSubscriptionToEventAnnouncements(enabled: boolean): Promise<void> {
+      try {
+        if (enabled) {
+          await PushNotificationHelper.subscribeToEventAnnouncements();
+        } else {
+          await PushNotificationHelper.unsubscribeFromEventAnnouncements();
+        }
+      } catch (error) {
+        throw new Error(
+          `Cannot update event announcements subscription as Firebase is unavailable; try restarting the app and make sure you have an internet connection. Details: ${error}`,
+        );
+      }
+      this.$state.subscribedToEventAnnouncements = enabled;
+      localStorage.setItem("piniaState-" + key, JSON.stringify(this.$state));
+    },
+    async setSubscriptionToEventUpdates(enabled: boolean): Promise<void> {
+      try {
+        if (enabled) {
+          await PushNotificationHelper.subscribeToEventUpdates();
+        } else {
+          await PushNotificationHelper.unsubscribeFromEventUpdates();
+        }
+      } catch (error) {
+        throw new Error(
+          `Cannot update event updates subscription as Firebase is unavailable; try restarting the app and make sure you have an internet connection. Details: ${error}`,
+        );
+      }
+      this.$state.subscribedToEventUpdates = enabled;
+      localStorage.setItem("piniaState-" + key, JSON.stringify(this.$state));
+    },
+    setLanguage(language: LanguageKey): void {
+      this.$state.selectedLanguage = language;
+      localStorage.setItem("piniaState-" + key, JSON.stringify(this.$state));
+      location = location;
+    }
+  },
+});
